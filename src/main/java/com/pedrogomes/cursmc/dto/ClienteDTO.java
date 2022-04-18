d589aae2b0304599a2c476ad35d988246a20a799
PedroGomes556 <pedrogomesps4xboxone@gmail.com>
2022-04-17 22:07:34 -0300

    Validação customizada: email não repetido na atualização do Cliente

--- src/main/java/com/pedrogomes/cursmc/dto/ClienteDTO.java
+++ src/main/java/com/pedrogomes/cursmc/dto/ClienteDTO.java
@@ -2,14 +2,15 @@ package com.pedrogomes.cursmc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.Email;
-
 import com.pedrogomes.cursmc.domain.Cliente;
+import com.pedrogomes.cursmc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
