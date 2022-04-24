5ebb9675071e1d51d91054d5b9ccdc2405be5742
PedroGomes556 <pedrogomesps4xboxone@gmail.com>
2022-04-23 22:00:05 -0300

    Criando profile de teste

---
+++ src/main/java/com/pedrogomes/cursmc/config/TesConfig.java
@@ -0,0 +1,27 @@
+package com.pedrogomes.cursmc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.pedrogomes.cursmc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TesConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean 
+	public boolean instantiateDatabase() throws ParseException {
+		
+		dbService.instantiateTestDatabase();
+		
+		return true;
+	}
+	
+}
