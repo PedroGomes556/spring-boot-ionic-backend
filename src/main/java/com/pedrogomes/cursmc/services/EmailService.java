50cbc199fded86da2fba56bb3d4708e71970efef
PedroGomes556 <pedrogomesps4xboxone@gmail.com>
2022-07-12 11:37:44 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/pedrogomes/cursmc/services/EmailService.java
+++ src/main/java/com/pedrogomes/cursmc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.pedrogomes.cursmc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.pedrogomes.cursmc.domain.Pedido;
@@ -9,5 +11,8 @@ public interface EmailService {
 	void senderOrderConfirmationEmail(Pedido pedido);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 	
 }
